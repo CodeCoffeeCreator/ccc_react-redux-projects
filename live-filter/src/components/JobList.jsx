5b47affbafb250de256e37c06d2b8940fd78e6ce
Denny Pearce <digital.y.coffee@gmail.com>
2022-06-12 23:58:17 +0300

    modify the files

--- live-filter/src/components/JobList.jsx
+++ live-filter/src/components/JobList.jsx
@@ -1,14 +1,32 @@
-import data from '../mock/data.json';
+import { useSelector, useDispatch } from 'react-redux';
 import { JobPosition } from './JobPosition';
+import { selectVisiblePositions } from 'store/positions/position-selectors';
+import { selectFitlers } from 'store/filters/filter-selectors';
+
+import { addFilter } from 'store/filters/filter-actions';
 
 const JobList = () => {
+  const dispatch = useDispatch();
+  const currentFitlers = useSelector(selectFitlers);
+  const positions = useSelector((state) =>
+    selectVisiblePositions(state, currentFitlers)
+  );
+
+  const handleAddFilter = (filter) => {
+    dispatch(addFilter(filter));
+  };
+
   return (
     <div className='job-list'>
-      {data.map(item => (
-        <JobPosition key={item.id} {...item} />
+      {positions.map((item) => (
+        <JobPosition
+          key={item.id}
+          handleAddFilter={handleAddFilter}
+          {...item}
+        />
       ))}
     </div>
-  )
-}
+  );
+};
 
-export {JobList};
+export { JobList };
