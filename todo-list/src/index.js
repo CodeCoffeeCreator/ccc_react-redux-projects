7ec60767abc86103b3cb63ba06e711c5b8fd3fb9
Denny Pearce <digital.y.coffee@gmail.com>
2022-07-22 12:22:12 +0300

    remove extra project - todo list; because it's newby project

--- todo-list/src/index.js
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom';
-import { cofigureStore } from './store';
-import { Root } from './Root';
-
-const store = cofigureStore();
-
-const rootElement = document.getElementById('root');
-ReactDOM.render(<Root store={store} />, rootElement);
