d0e8b1cda76582260a8e4ba28a65527b2cbfd02d
Denny Pearce <digital.y.coffee@gmail.com>
2022-07-23 22:13:13 +0300

    rewrite some code into components

--- country-parser-two-themed/src/components/Header.jsx
+++ country-parser-two-themed/src/components/Header.jsx
@@ -1,9 +1,12 @@
 import styled from 'styled-components';
+import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { IoMoon, IoMoonOutline } from 'react-icons/io5';
 
 import { Container } from './Container';
+import { setTheme } from '../store/theme/theme-actions';
+import { clearControls } from '../store/controls/controls-actions';
 
 const HeaderEl = styled.header`
   box-shadow: var(--shadow);
@@ -30,12 +33,17 @@ const ModeSwitcher = styled.div`
   color: var(--colors-text);
   font-size: var(--fs-sm);
   cursor: pointer;
-  // font-weight: var(--fw-bold);
   text-transform: capitalize;
 `;
 
 export const Header = () => {
-  const theme = 'light';
+  const dispatch = useDispatch();
+  const theme = useSelector((state) => state.theme);
+
+  const toggleTheme = () =>
+    dispatch(setTheme(theme === 'light' ? 'dark' : 'light'));
+
+  const cleanUp = () => dispatch(clearControls());
 
   useEffect(() => {
     document.body.setAttribute('data-theme', theme);
@@ -45,8 +53,8 @@ export const Header = () => {
     <HeaderEl>
       <Container>
         <Wrapper>
-          <Title>Where is the world?</Title>
-          <ModeSwitcher>
+          <Title onClick={cleanUp}>Where is the world?</Title>
+          <ModeSwitcher onClick={toggleTheme}>
             {theme === 'light' ? (
               <IoMoonOutline size='14px' />
             ) : (
